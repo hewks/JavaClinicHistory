9418add2570913b8030c50f585ec0540a1d2d4bf
Carual <43898364+Carual@users.noreply.github.com>
2019-07-06 15:46:30 -0500

    Add files via upload

---
+++ src/Clases/Informacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Juan
+ */
+public class Informacion {
+    protected static final String DATABASE="MedSystemDataBase";
+    protected static final String DRIVER="com.mysql.jdbc.Driver";
+    protected static final String USER="admin";
+    protected static final String URL="jdbc:mysql://sl-us-south-1-portal.48.dblayer.com";
+    protected static final String PASS="Asdfghjkl159753";
+    protected static final String PORT="24186";
+    protected static final String ARG="?useSSL=false";
+    protected String user_database="users";
+    protected String client_database="";
+    
+
+    
+    
+}
